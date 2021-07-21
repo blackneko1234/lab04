756bdf9f9aa8ff675c4fe225900980e8ea12a24f
Decha_L <blackneko1234@hotmail.com>
2021-07-21 21:33:09 +0700

    3
    3

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,10 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import EventList from '../views/EventList.vue'
 import About from '../views/About.vue'
-import EventDetails from '../views/EventDetails.vue'
+import EventDetails from '../views/event/Details.vue'
+import EventRegister from '../views/event/Register.vue'
+import EventEdit from '../views/event/Edit.vue'
+import EventLayout from '../views/event/Layout.vue'
 
 const routes = [
   {
@@ -14,11 +17,31 @@ const routes = [
     path: '/about',
     name: 'About',
     component: About
-  }, {
+  },
+  {
     path: '/event/:id',
-    name: 'EventDetails',
-    component: EventDetails,
-    props: true
+    name: 'EventLayout',
+    props: true,
+    component: EventLayout,
+    children: [
+      {
+        path: '',
+        name: 'EventDetails',
+        component: EventDetails
+      },
+      {
+        path: 'register',
+        name: 'EventRegister',
+        props: true,
+        component: EventRegister
+      },
+      {
+        path: 'edit',
+        name: 'EventEdit',
+        props: true,
+        component: EventEdit
+      }
+    ]
   }
 ]
 
